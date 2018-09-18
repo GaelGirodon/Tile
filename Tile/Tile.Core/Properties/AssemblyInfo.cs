7a2c2d92aaf3666df57b25658072a50bee771a98
Gael Girodon <git@gaelgirodon.fr>
2018-09-18 21:38:03 +0200

    Bump the version number to 0.8.0

--- Tile/Tile.Core/Properties/AssemblyInfo.cs
+++ Tile/Tile.Core/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 

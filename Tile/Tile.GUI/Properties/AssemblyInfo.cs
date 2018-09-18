e3b2d402304af8d716cd3331f9a4af54dabe682a
Gael Girodon <git@gaelgirodon.fr>
2018-09-18 21:14:44 +0200

    Update the application description

--- Tile/Tile.GUI/Properties/AssemblyInfo.cs
+++ Tile/Tile.GUI/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tile")]
-[assembly: AssemblyDescription("A simple Windows utility to generate and set up the start menu tiles")]
+[assembly: AssemblyDescription("A simple Windows utility to customize the start menu tiles")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gaël Girodon")]
 [assembly: AssemblyProduct("Tile")]
